8f2bf82cd45c43abf136748134e2fb38a4e288f8
Mekarosi <mekarosi_assa@yahoo.com>
2022-09-03 10:47:39 -0700

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
    devise_scope :user do  
    get '/users/sign_out' => 'devise/sessions#destroy'     
 end
+
+resources :users do
+  resource :profile
+end
  get 'about', to: 'pages#about'
  resources :contacts, only: :create 
  get 'contact-us', to: 'contacts#new', as: 'new_contact'
