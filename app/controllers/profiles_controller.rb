06c42c7b628b0c16f1de5317be439f4de6dce5b1
Mekarosi <mekarosi_assa@yahoo.com>
2022-09-06 13:00:59 -0700

    Added before actions to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+   before_action :authenticate_user!
+   before_action :only_current_user
 
    # GET to /users/:user_id/profile/new
    def new
@@ -43,8 +45,13 @@ def update
 end  
 
 
-private
+ private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
   end	
+
+  def only_current_user
+    @user = User.find( params[:user_id] )
+    redirect_to(root_url) unless @user == current_user
+  end  
 end
